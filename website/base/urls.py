ef77fb4072c299dc446df007da666bcfaedbbdc5
Atul Sharma <41663027+atulsharma2000@users.noreply.github.com>
2021-06-17 16:10:29 +0530

    register form

--- website/base/urls.py
+++ website/base/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('',views.index, name="index"),
    path('login/',views.login, name="login"),
-   path('register/',views.register, name="register"),
+   path('register/',views.RegisterPage.as_view(), name="register"),
   
 ]
--- website/base/views.py
+++ website/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from . forms import UserCreationForm
+from django.views.generic.edit import CreateView, FormView
 # Create your views here.
 
 
@@ -11,7 +12,16 @@ def index(request):
 def login(request):
     return render(request, 'base/login.html',{})
 
-def register(request):
-    return render(request, 'base/register.html',{})
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
 
 
